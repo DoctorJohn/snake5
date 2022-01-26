cc5edadbe08c37c585376672dc543187b516974a
Jonathan Ehwald <github@ehwald.info>
2022-01-26 04:41:15 +0100

    feat: basic drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+type Vec2 = [number, number];
+
+const GRID_SIZE: Vec2 = [10, 10]; // units
+const TILE_SIZE: Vec2 = [50, 50]; // pixels
+const TILE_VARIANT0_COLOR = "wheat";
+const TILE_VARIANT1_COLOR = "white";
+const SNAKE_BODY_COLOR = "lime";
+const SNAKE_HEAD_COLOR = "green";
+const FRUIT_COLOR = "red";
+
+const TEST_SNAKE: Vec2[] = [
+  [2, 2],
+  [1, 2],
+  [1, 1],
+];
+
+const TEST_FRUIT: Vec2 = [4, 4];
 
 function App() {
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+
+  function drawBoard(context: CanvasRenderingContext2D) {
+    for (var x = 0; x <= GRID_SIZE[0]; x++) {
+      for (var y = 0; y <= GRID_SIZE[0]; y++) {
+        context.beginPath();
+        context.rect(
+          x * TILE_SIZE[0],
+          y * TILE_SIZE[1],
+          TILE_SIZE[0],
+          TILE_SIZE[1]
+        );
+
+        const fillVariant = (x + y) % 2;
+        context.fillStyle = fillVariant
+          ? TILE_VARIANT0_COLOR
+          : TILE_VARIANT1_COLOR;
+        context.fill();
+      }
+    }
+  }
+
+  function drawSnake(context: CanvasRenderingContext2D, snake: Vec2[]) {
+    // Draw snake
+    for (const snakePart of snake) {
+      context.beginPath();
+      context.rect(
+        snakePart[0] * TILE_SIZE[0],
+        snakePart[1] * TILE_SIZE[1],
+        TILE_SIZE[0],
+        TILE_SIZE[1]
+      );
+      context.fillStyle = SNAKE_BODY_COLOR;
+      context.fill();
+    }
+
+    // Draw head again
+    const snakeHead = snake[0];
+    context.beginPath();
+    context.rect(
+      snakeHead[0] * TILE_SIZE[0],
+      snakeHead[1] * TILE_SIZE[1],
+      TILE_SIZE[0],
+      TILE_SIZE[1]
+    );
+    context.fillStyle = SNAKE_HEAD_COLOR;
+    context.fill();
+  }
+
+  function drawFruit(context: CanvasRenderingContext2D, fruit: Vec2) {
+    context.beginPath();
+    context.rect(
+      fruit[0] * TILE_SIZE[0],
+      fruit[1] * TILE_SIZE[1],
+      TILE_SIZE[0],
+      TILE_SIZE[1]
+    );
+    context.fillStyle = FRUIT_COLOR;
+    context.fill();
+  }
+
+  React.useEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
+
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    if (!context) {
+      return;
+    }
+
+    drawBoard(context);
+    drawSnake(context, TEST_SNAKE);
+    drawFruit(context, TEST_FRUIT);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <canvas
+        className="canvas"
+        width={GRID_SIZE[0] * TILE_SIZE[0]}
+        height={GRID_SIZE[1] * TILE_SIZE[1]}
+        ref={canvasRef}
+      />
     </div>
   );
 }
