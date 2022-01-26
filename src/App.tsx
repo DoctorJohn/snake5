cf80e6a924700804cd5f814f31ea573d9bafc701
Jonathan Ehwald <github@ehwald.info>
2022-01-26 04:50:21 +0100

    refactor: use object based 2D types

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,37 @@
 import React from "react";
 import "./App.css";
 
-type Vec2 = [number, number];
+type Point = { x: number; y: number };
+type Size = { w: number; h: number };
 
-const GRID_SIZE: Vec2 = [10, 10]; // units
-const TILE_SIZE: Vec2 = [50, 50]; // pixels
+const GRID_SIZE: Size = { w: 10, h: 10 }; // units
+const TILE_SIZE: Size = { w: 50, h: 50 }; // pixels
 const TILE_VARIANT0_COLOR = "wheat";
 const TILE_VARIANT1_COLOR = "white";
 const SNAKE_BODY_COLOR = "lime";
 const SNAKE_HEAD_COLOR = "green";
 const FRUIT_COLOR = "red";
 
-const TEST_SNAKE: Vec2[] = [
-  [2, 2],
-  [1, 2],
-  [1, 1],
+const TEST_SNAKE: Point[] = [
+  { x: 2, y: 2 },
+  { x: 1, y: 2 },
+  { x: 1, y: 1 },
 ];
 
-const TEST_FRUIT: Vec2 = [4, 4];
+const TEST_FRUIT: Point = { x: 4, y: 4 };
 
 function App() {
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
 
   function drawBoard(context: CanvasRenderingContext2D) {
-    for (var x = 0; x <= GRID_SIZE[0]; x++) {
-      for (var y = 0; y <= GRID_SIZE[0]; y++) {
+    for (var x = 0; x <= GRID_SIZE.w; x++) {
+      for (var y = 0; y <= GRID_SIZE.h; y++) {
         context.beginPath();
         context.rect(
-          x * TILE_SIZE[0],
-          y * TILE_SIZE[1],
-          TILE_SIZE[0],
-          TILE_SIZE[1]
+          x * TILE_SIZE.w,
+          y * TILE_SIZE.h,
+          TILE_SIZE.w,
+          TILE_SIZE.h
         );
 
         const fillVariant = (x + y) % 2;
@@ -42,15 +43,15 @@ function App() {
     }
   }
 
-  function drawSnake(context: CanvasRenderingContext2D, snake: Vec2[]) {
+  function drawSnake(context: CanvasRenderingContext2D, snake: Point[]) {
     // Draw snake
     for (const snakePart of snake) {
       context.beginPath();
       context.rect(
-        snakePart[0] * TILE_SIZE[0],
-        snakePart[1] * TILE_SIZE[1],
-        TILE_SIZE[0],
-        TILE_SIZE[1]
+        snakePart.x * TILE_SIZE.w,
+        snakePart.y * TILE_SIZE.h,
+        TILE_SIZE.w,
+        TILE_SIZE.h
       );
       context.fillStyle = SNAKE_BODY_COLOR;
       context.fill();
@@ -60,22 +61,22 @@ function App() {
     const snakeHead = snake[0];
     context.beginPath();
     context.rect(
-      snakeHead[0] * TILE_SIZE[0],
-      snakeHead[1] * TILE_SIZE[1],
-      TILE_SIZE[0],
-      TILE_SIZE[1]
+      snakeHead.x * TILE_SIZE.w,
+      snakeHead.y * TILE_SIZE.h,
+      TILE_SIZE.w,
+      TILE_SIZE.h
     );
     context.fillStyle = SNAKE_HEAD_COLOR;
     context.fill();
   }
 
-  function drawFruit(context: CanvasRenderingContext2D, fruit: Vec2) {
+  function drawFruit(context: CanvasRenderingContext2D, fruit: Point) {
     context.beginPath();
     context.rect(
-      fruit[0] * TILE_SIZE[0],
-      fruit[1] * TILE_SIZE[1],
-      TILE_SIZE[0],
-      TILE_SIZE[1]
+      fruit.x * TILE_SIZE.w,
+      fruit.y * TILE_SIZE.h,
+      TILE_SIZE.w,
+      TILE_SIZE.h
     );
     context.fillStyle = FRUIT_COLOR;
     context.fill();
@@ -102,8 +103,8 @@ function App() {
     <div>
       <canvas
         className="canvas"
-        width={GRID_SIZE[0] * TILE_SIZE[0]}
-        height={GRID_SIZE[1] * TILE_SIZE[1]}
+        width={GRID_SIZE.w * TILE_SIZE.w}
+        height={GRID_SIZE.h * TILE_SIZE.h}
         ref={canvasRef}
       />
     </div>
